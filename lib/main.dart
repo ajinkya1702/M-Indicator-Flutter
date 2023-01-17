06b119c4a3297351482916359d29945ac646b995
ajinkya1702 <ajinkyamorankar03@gmail.com>
2023-01-17 19:33:48 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: camel_case_types, prefer_const_constructors, avoid_unnecessary_containers, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-  const myApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome"),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
